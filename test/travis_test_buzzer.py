d8594f3f69dc831478f51e88377fa5e32fe814a3
somalai <somalai@gmail.com>
2018-11-15 10:00:45 +0800

    Comment out source codes that will be added later

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -8,9 +8,9 @@ from std_msgs.msg import UInt16
 
 class BuzzerTest(unittest.TestCase):
 
-    def setUp(self):                                #setUpメソッドを追加する
-        self.client = actionlib.SimpleActionClient("music", MusicAction)
-        self.device_values = []
+#    def setUp(self):                                #setUpメソッドを追加する
+#       self.client = actionlib.SimpleActionClient("music", MusicAction)
+#       self.device_values = []
 
     def test_node_exist(self):
         nodes = rosnode.get_node_names()

8a679458c8090bf78df8b94f6d53b026661f5141
somalai <somalai@gmail.com>
2018-11-15 09:56:42 +0800

    Modify travis_test_buzzer.py for current test

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -4,7 +4,7 @@ import rospy, unittest, rostest, actionlib
 import rosnode
 import time
 from std_msgs.msg import UInt16
-from raspimouse_ros_2.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal #1行追加
+#from raspimouse_ros_2.msg import MusicAction, MusicResult, MusicFeedback, MusicGoal #1行追加
 
 class BuzzerTest(unittest.TestCase):
 
@@ -26,35 +26,36 @@ class BuzzerTest(unittest.TestCase):
             data = f.readline()
             self.assertEqual(data,"1234\n","value does not written to rtbuzzer0")
 
-    def test_music(self):
-        ### skip this test because a problem of this test code ###
-        return
-        goal = MusicGoal()
-        goal.freqs = [100, 200, 300, 0]
-        goal.durations = [2,2,2,2]
+ #   def test_music(self):
+ #       ### skip this test because a problem of this test code ###
+ #       return
+ #       goal = MusicGoal()
+ #       goal.freqs = [100, 200, 300, 0]
+ #       goal.durations = [2,2,2,2]
 
-        self.client.wait_for_server()
-        self.client.send_goal(goal,feedback_cb=self.feedback_cb)
-        self.client.wait_for_result()
+ #       self.client.wait_for_server()
+ #       self.client.send_goal(goal,feedback_cb=self.feedback_cb)
+ #       self.client.wait_for_result()
 
-        self.assertTrue(self.client.get_result(),"invalid result")
-        self.assertEqual(goal.freqs,self.device_values,"invalid feedback:"
-                + ",".join([str(e) for e in self.device_values]))
+ #      self.assertTrue(self.client.get_result(),"invalid result")
+ #       self.assertEqual(goal.freqs,self.device_values,"invalid feedback:"
+ #               + ",".join([str(e) for e in self.device_values]))
 
         ###preemption###
-        self.device_values = []
-        self.client.send_goal(goal,feedback_cb=self.feedback_cb)
-        self.client.wait_for_result(rospy.Duration.from_sec(0.5))
+ #       self.device_values = []
+ #       self.client.send_goal(goal,feedback_cb=self.feedback_cb)
+ #       self.client.wait_for_result(rospy.Duration.from_sec(0.5))
 
-        self.assertFalse(self.client.get_result(),"stop is requested but return true")
-        self.assertFalse(goal.freqs == self.device_values,"not stopped")
+ #      self.assertFalse(self.client.get_result(),"stop is requested but return true")
+ #       self.assertFalse(goal.freqs == self.device_values,"not stopped")
 
-    def feedback_cb(self,feedback):
-        with open("/dev/rtbuzzer0","r") as f:
-            data = f.readline()
-            self.device_values.append(int(data.rstrip()))
+ #   def feedback_cb(self,feedback):
+ #       with open("/dev/rtbuzzer0","r") as f:
+ #           data = f.readline()
+ #           self.device_values.append(int(data.rstrip()))
 
 if __name__ == '__main__':
     time.sleep(3)
     rospy.init_node('travis_test_buzzer')
-    rostest.rosrun('raspimouse_ros_2','travis_test_buzzer',BuzzerTest)
+    rostest.rosrun('pimouse_ros','travis_test_buzzer',BuzzerTest)
+#    rostest.rosrun('raspimouse_ros_2','travis_test_buzzer',BuzzerTest)

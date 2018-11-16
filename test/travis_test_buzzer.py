67b09e2d44923b95c6674482b246971127d64c05
somalai <somalai@gmail.com>
2018-11-16 10:01:44 +0800

    Use space instead of tab for indentation

--- test/travis_test_buzzer.py
+++ test/travis_test_buzzer.py
@@ -15,9 +15,9 @@ class BuzzerTest(unittest.TestCase):
         for i in range(10):
             pub.publish(1234)
             time.sleep(0.1)	
-		with open("/dev/rtbuzzer0", "r") as f:
-			data = f.readline()
-			self.assertEqual(data, "1234\n", "value is not written to rtbuzzer0") 
+        with open("/dev/rtbuzzer0", "r") as f:
+            data = f.readline()
+            self.assertEqual(data, "1234\n", "value is not written to rtbuzzer0") 
 
 if __name__ == '__main__':
     time.sleep(3)
